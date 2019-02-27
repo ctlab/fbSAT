aa75c6ee347db4e7457b99b0464deec9ea4c2599
Konstantin Chukharev <lipen00@gmail.com>
2019-02-27 14:58:34 +0300

    Make ScenarioElement simple class and define custom equals and hashCode

--- src/main/kotlin/ru/ifmo/fbsat/scenario/ScenarioElement.kt
+++ src/main/kotlin/ru/ifmo/fbsat/scenario/ScenarioElement.kt
@@ -1,6 +1,6 @@
 package ru.ifmo.fbsat.scenario
 
-data class ScenarioElement(
+class ScenarioElement(
     val inputEvent: String,
     val inputValues: String,
     val outputEvent: String?,
@@ -11,5 +11,27 @@ data class ScenarioElement(
 
     override fun toString(): String {
         return "Element($inputEvent[$inputValues] / $outputEvent[$outputValues])"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ScenarioElement
+
+        if (inputEvent != other.inputEvent) return false
+        if (inputValues != other.inputValues) return false
+        if (outputEvent != other.outputEvent) return false
+        if (outputValues != other.outputValues) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = inputEvent.hashCode()
+        result = 31 * result + inputValues.hashCode()
+        result = 31 * result + (outputEvent?.hashCode() ?: 0)
+        result = 31 * result + outputValues.hashCode()
+        return result
     }
 }
