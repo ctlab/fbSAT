4889c358a9b67a7ed8e687b89e875f5b03acd083
Konstantin Chukharev <lipen00@gmail.com>
2019-02-27 15:07:48 +0300

    Remove unnecessary helpers

--- src/main/kotlin/ru/ifmo/fbsat/utils/Helpers.kt
+++ src/main/kotlin/ru/ifmo/fbsat/utils/Helpers.kt
@@ -1,22 +1,5 @@
 package ru.ifmo.fbsat.utils
 
-fun <T, R> Iterable<T>.reductions(
-    initial: R,
-    operation: (acc: R, T) -> R
-): Sequence<R> = sequence {
-    var cur = initial
-    forEach {
-        cur = operation(cur, it)
-        yield(cur)
-    }
-}
-
-// fun <T> Array<T>.mapInPlace(transform: (T) -> T) {
-//    for (i in this.indices) {
-//        this[i] = transform(this[i])
-//    }
-// }
-
 fun String.toBooleanArray(): BooleanArray {
     return this.map {
         when (it) {
