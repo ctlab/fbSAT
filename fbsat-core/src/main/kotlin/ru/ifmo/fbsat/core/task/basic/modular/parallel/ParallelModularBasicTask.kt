ae67dde342c96a6c6c8544bfd0a10e17721c94da
Konstantin Chukharev <lipen00@gmail.com>
2019-08-29 14:34:28 +0300

    Remove commented code

--- fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/task/basic/modular/parallel/ParallelModularBasicTask.kt
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/task/basic/modular/parallel/ParallelModularBasicTask.kt
@@ -105,47 +105,4 @@ class ParallelModularBasicTask(
     fun finalize2() {
         solver.finalize2()
     }
-
-    // @Suppress("LocalVariableName")
-    // private fun Solver.declareCardinality() {
-    //     if (maxTransitions == null) return
-    //
-    //     val T: Int by context(maxTransitions)
-    //     val totalizer: IntArray = context.computeIfAbsent("_totalizerBasic") {
-    //         val transition: IntMultiArray by context
-    //         declareTotalizer(sequence {
-    //             val (M, C, K, _) = transition.shape
-    //             for (m in 1..M)
-    //                 for (c in 1..C)
-    //                     for (k in 1..K)
-    //                         yield(-transition[m, c, k, C + 1])
-    //         })
-    //     } as IntArray
-    //     val declaredT: Int? = context["_declaredT"] as Int?
-    //     solver.declareComparatorLessThanOrEqual(totalizer, T, declaredT)
-    //     context["_declaredT"] = T
-    // }
-    //
-    // private fun Solver.updateCardinality(newMaxTransitions: Int?) {
-    //     val vars = Unit
-    //     with(vars) {
-    //         maxTransitions?.let { T ->
-    //             check(newMaxTransitions != null && newMaxTransitions <= T) { "Cannot soften UB" }
-    //         }
-    //
-    //         if (newMaxTransitions == null && !ru.ifmo.fbsat.core.utils.Globals.IS_ENCODE_TOTALIZER) return
-    //         if (totalizer == null) {
-    //             totalizer = declareTotalizer(sequence {
-    //                 for (m in 1..M)
-    //                     for (c in 1..C)
-    //                         for (k in 1..K)
-    //                             yield(-transition[m, c, k, C + 1])
-    //             })
-    //         }
-    //         if (newMaxTransitions == null) return
-    //
-    //         declareComparatorLessThanOrEqual(totalizer!!, newMaxTransitions, maxTransitions)
-    //         maxTransitions = newMaxTransitions
-    //     }
-    // }
 }
