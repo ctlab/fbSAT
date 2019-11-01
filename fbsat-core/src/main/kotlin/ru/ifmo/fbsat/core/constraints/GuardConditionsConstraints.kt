104b9c0c6e5b56217011aa1a4cfcccf4b3894e18
Konstantin Chukharev <lipen00@gmail.com>
2019-11-01 15:57:36 +0300

    Fix guard constraints

--- fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/constraints/GuardConditionsConstraints.kt
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/constraints/GuardConditionsConstraints.kt
@@ -12,6 +12,7 @@ import ru.ifmo.fbsat.core.solver.implyImply
 import ru.ifmo.fbsat.core.solver.implyImplyIff
 import ru.ifmo.fbsat.core.solver.implyImplyIffAnd
 import ru.ifmo.fbsat.core.solver.implyImplyIffOr
+import ru.ifmo.fbsat.core.solver.implyOr
 import ru.ifmo.fbsat.core.task.single.complete.CompleteVariables
 import ru.ifmo.fbsat.core.task.single.extended.ExtendedVariables
 import ru.ifmo.fbsat.core.utils.boolToSign
@@ -22,6 +23,7 @@ fun Solver.declarePositiveGuardConditionsConstraints(extendedVars: ExtendedVaria
         comment("Positive guard conditions constraints: inputless")
         declareGuardConditionsConstraintsInputless(
             C = C, K = K, P = P, X = X,
+            transitionDestination = transitionDestination,
             nodeType = nodeType,
             nodeInputVariable = nodeInputVariable,
             nodeParent = nodeParent,
@@ -49,8 +51,8 @@ fun Solver.declareNegativeGuardConditionsConstraints(completeVars: CompleteVaria
     with(completeVars) {
         // Note: no inputless constraints
 
+        // Note: be very careful with positive/negative variables!
         for (u in Us) {
-            // Note: be very careful with positive/negative variables!
             comment("Negative guard conditions constraints: for u = $u")
             declareGuardConditionsConstraintsForInput(
                 u = u,
@@ -71,6 +73,7 @@ fun Solver.declareGuardConditionsConstraintsInputless(
     K: Int,
     P: Int,
     X: Int,
+    transitionDestination: IntMultiArray,
     nodeType: IntMultiArray,
     nodeInputVariable: IntMultiArray,
     nodeParent: IntMultiArray,
@@ -86,18 +89,17 @@ fun Solver.declareGuardConditionsConstraintsInputless(
                     nodeType[c, k, p + 1, NodeType.NONE.value]
                 )
 
-    comment("Only terminal nodes have associated input variables")
-    // (nodeType[p] = TERMINAL) <=> (nodeInputVariable[p] != 0)
+    comment("Only null-transitions have no guard (root is none-typed)")
+    // (transitionDestination = 0) <=> (nodeType[1] = NONE)
     for (c in 1..C)
         for (k in 1..K)
-            for (p in 1..P)
-                iff(
-                    nodeType[c, k, p, NodeType.TERMINAL.value],
-                    -nodeInputVariable[c, k, p, X + 1]
-                )
+            iff(
+                transitionDestination[c, k, C + 1],
+                nodeType[c, k, 1, NodeType.NONE.value]
+            )
 
     comment("Child-parent relation")
-    // (nodeChild[p] = ch) => (nodeParent[ch] = p) :: ch > p
+    // (nodeChild[p] = ch) => (nodeParent[ch] = p)
     for (c in 1..C)
         for (k in 1..K)
             for (p in 1..P)
@@ -106,6 +108,32 @@ fun Solver.declareGuardConditionsConstraintsInputless(
                         nodeChild[c, k, p, ch],
                         nodeParent[c, k, ch, p]
                     )
+
+    comment("Parent-child relation")
+    // (nodeParent[ch] = p) => (nodeChild[p] = ch) | (nodeChild[p] = ch-1)
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                for (ch in (p + 1)..P)
+                    implyOr(
+                        nodeParent[c, k, ch, p],
+                        nodeChild[c, k, p, ch],
+                        nodeChild[c, k, p, ch - 1]
+                    )
+
+    comment("Parent has lesser number")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                for (par in p..P)
+                    clause(-nodeParent[c, k, p, par])
+
+    comment("Child has greater number")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                for (ch in 1..p)
+                    clause(-nodeChild[c, k, p, ch])
 
     comment("Only typed nodes, except the root, have parents")
     for (c in 1..C)
@@ -116,18 +144,128 @@ fun Solver.declareGuardConditionsConstraintsInputless(
                     -nodeType[c, k, p, NodeType.NONE.value]
                 )
 
+    // NONE-TYPE NODES CONSTRAINTS
+
+    comment("None-typed nodes do not have children")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                imply(
+                    nodeType[c, k, p, NodeType.NONE.value],
+                    nodeChild[c, k, p, P + 1]
+                )
+
+    // TERMINALS CONSTRAINTS
+
+    comment("Only terminal nodes have associated input variables")
+    // (nodeType[p] = TERMINAL) <=> (nodeInputVariable[p] != 0)
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                iff(
+                    nodeType[c, k, p, NodeType.TERMINAL.value],
+                    -nodeInputVariable[c, k, p, X + 1]
+                )
+
+    comment("Terminals do not have children")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                imply(
+                    nodeType[c, k, p, NodeType.TERMINAL.value],
+                    nodeChild[c, k, p, P + 1]
+                )
+
+    // AND/OR NODES CONSTRAINTS
+
+    comment("11.0a. AND/OR nodes cannot have numbers P-1 or P")
+    for (c in 1..C)
+        for (k in 1..K) {
+            if (P >= 1) {
+                clause(-nodeType[c, k, P, NodeType.AND.value])
+                clause(-nodeType[c, k, P, NodeType.OR.value])
+            }
+            if (P >= 2) {
+                clause(-nodeType[c, k, P - 1, NodeType.AND.value])
+                clause(-nodeType[c, k, P - 1, NodeType.OR.value])
+            }
+        }
+
+    comment("11.0b. AND/OR: left child cannot have number P")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..(P - 2))
+                for (nt in listOf(NodeType.AND, NodeType.OR))
+                    imply(
+                        nodeType[c, k, p, nt.value],
+                        -nodeChild[c, k, p, P]
+                    )
+
+    comment("11.1. AND/OR nodes have left child")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..(P - 2))
+                for (t in listOf(NodeType.AND, NodeType.OR))
+                    imply(
+                        nodeType[c, k, p, t.value],
+                        -nodeChild[c, k, p, P + 1]
+                    )
+
+    comment("11.3. AND/OR: hard to explain")
+    // parent[p, par] & nodetype[par, AND/OR] => child[par, p] | child[par, p-1]
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 3..P)
+                for (par in 1..(p - 2))
+                    for (nt in listOf(NodeType.AND, NodeType.OR))
+                        clause(
+                            -nodeParent[c, k, p, par],
+                            -nodeType[c, k, par, nt.value],
+                            nodeChild[c, k, par, p],
+                            nodeChild[c, k, par, p - 1]
+                        )
+
     comment("Right child of binary operators follows the left one")
     // (nodeType[p] in {AND,OR}) & (nodeChild[p] = ch) => (nodeParent[ch+1] = p)
     for (c in 1..C)
         for (k in 1..K)
             for (p in 1..P)
-                for (ch in (p + 1)..P)
-                    for (t in listOf(NodeType.AND, NodeType.OR).map { it.value })
+                for (ch in (p + 1) until P)
+                    for (t in listOf(NodeType.AND, NodeType.OR))
                         implyImply(
-                            nodeType[c, k, p, t],
+                            nodeType[c, k, p, t.value],
                             nodeChild[c, k, p, ch],
                             nodeParent[c, k, ch + 1, p]
                         )
+
+    // NOT NODES CONSTRAINTS
+
+    comment("12.0. NOT nodes cannot have number P")
+    for (c in 1..C)
+        for (k in 1..K)
+            if (P >= 1)
+                clause(-nodeType[c, k, P, NodeType.NOT.value])
+
+    comment("12.1. NOT nodes have left child")
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1 until P)
+                imply(
+                    nodeType[c, k, p, NodeType.NOT.value],
+                    -nodeChild[c, k, p, P + 1]
+                )
+
+    comment("12.2. NOT: parent's child is the current node")
+    // parent[p, par] & nodetype[par, NOT] => child[par, p]
+    for (c in 1..C)
+        for (k in 1..K)
+            for (p in 1..P)
+                for (par in 1 until p)
+                    implyImply(
+                        nodeParent[c, k, p, par],
+                        nodeType[c, k, par, NodeType.NOT.value],
+                        nodeChild[c, k, par, p]
+                    )
 }
 
 fun Solver.declareGuardConditionsConstraintsForInput(
@@ -171,7 +309,7 @@ fun Solver.declareGuardConditionsConstraintsForInput(
             for (p in 1..P)
                 for (ch in (p + 1) until P)
                     implyImplyIffAnd(
-                        nodeType[c, k, p, NodeType.OR.value],
+                        nodeType[c, k, p, NodeType.AND.value],
                         nodeChild[c, k, p, ch],
                         nodeValue[c, k, p, u],
                         nodeValue[c, k, ch, u],
@@ -199,9 +337,9 @@ fun Solver.declareGuardConditionsConstraintsForInput(
     for (c in 1..C)
         for (k in 1..K)
             for (p in 1..P)
-                for (ch in (p + 1) until P)
+                for (ch in (p + 1)..P)
                     implyImplyIff(
-                        nodeType[c, k, p, NodeType.OR.value],
+                        nodeType[c, k, p, NodeType.NOT.value],
                         nodeChild[c, k, p, ch],
                         nodeValue[c, k, p, u],
                         -nodeValue[c, k, ch, u]
