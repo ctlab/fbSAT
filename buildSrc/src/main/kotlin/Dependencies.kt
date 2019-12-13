6eb26192582c2fcd02b38be4b773b110d28aec7a
Konstantin Chukharev <lipen00@gmail.com>
2019-12-13 21:31:51 +0300

    Rename kotlin-xml-builder dependency

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions {
     const val okio = "2.4.1"
     const val gradle_versions = "0.27.0"
     const val mordant = "1.2.1"
-    const val kotlin_xml_builder = "1.5.3"
+    const val xml_builder = "1.5.3"
     const val lazycache = "0.3.0"
     const val klock = "1.7.5"
 }
@@ -25,7 +25,7 @@ object Libs {
     const val multiarray = "com.github.Lipen:MultiArray:${Versions.multiarray}"
     const val okio = "com.squareup.okio:okio:${Versions.okio}"
     const val mordant = "com.github.ajalt:mordant:${Versions.mordant}"
-    const val kotlin_xml_builder = "org.redundent:kotlin-xml-builder:${Versions.kotlin_xml_builder}"
+    const val xml_builder = "org.redundent:kotlin-xml-builder:${Versions.xml_builder}"
     const val lazycache = "com.github.Lipen:kotlin-lazycache:${Versions.lazycache}"
     const val klock = "com.soywiz.korlibs.klock:klock-jvm:${Versions.klock}"
 }
