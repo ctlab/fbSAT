e1eb4372e64ab7084332c5634321bed1b3ccf102
Konstantin Chukharev <lipen00@gmail.com>
2020-01-16 03:35:45 +0300

    Change return type of Iterable.withIndex to Sequence

--- fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/utils/Helpers.kt
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/utils/Helpers.kt
@@ -132,9 +132,8 @@ val <T> T.exhaustive: T
 
 inline fun <reified T> mutableListOfNulls(size: Int): MutableList<T?> = MutableList(size) { null }
 
-fun <T> Iterable<T>.withIndex(start: Int): Iterable<Pair<Int, T>> {
-    return asSequence().mapIndexed { index, value -> (start + index) to value }.asIterable()
-}
+fun <T> Iterable<T>.withIndex(start: Int): Sequence<Pair<Int, T>> =
+    asSequence().mapIndexed { index, value -> (start + index) to value }
 
 fun <T> Iterable<T>.firstIndexed(predicate: (Int, T) -> Boolean): T =
     withIndex().first { (index, value) -> predicate(index, value) }.value
