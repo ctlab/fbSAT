5a3aa633d06325f737096a0e4a13f25b616f0e86
Konstantin Chukharev <lipen00@gmail.com>
2020-01-29 23:08:45 +0300

    Add missing util functions to make cnf/dnf strings

---
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/utils/CnfDnf.kt
@@ -0,0 +1,22 @@
+package ru.ifmo.fbsat.core.utils
+
+private fun List<String>.joinToStringAutoBrace(separator: String): String {
+    val s = joinToString(separator)
+    return if (size > 1) "($s)" else s
+}
+
+fun makeDnfString(
+    terms: List<List<String>>,
+    conjunction: String = "&",
+    disjunction: String = "|"
+): String =
+    if (terms.isEmpty()) "0"
+    else terms.joinToString(disjunction) { it.joinToString(conjunction) }
+
+fun makeCnfString(
+    terms: List<List<String>>,
+    conjunction: String = "&",
+    disjunction: String = "|"
+): String =
+    if (terms.isEmpty()) "1"
+    else terms.joinToString(conjunction) { it.joinToStringAutoBrace(disjunction) }
