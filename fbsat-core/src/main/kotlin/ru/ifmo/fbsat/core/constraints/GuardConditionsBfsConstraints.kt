a4cb8684e7417d1940ea0ba96c3bb0be17cbb27b
Konstantin Chukharev <lipen00@gmail.com>
2020-03-13 18:04:55 +0300

    Simplify guard condition BFS constraints

--- fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/constraints/GuardConditionsBfsConstraints.kt
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/constraints/GuardConditionsBfsConstraints.kt
@@ -1,11 +1,7 @@
 package ru.ifmo.fbsat.core.constraints
 
 import ru.ifmo.fbsat.core.solver.Solver
-import ru.ifmo.fbsat.core.solver.Solver.Companion.falseVariable
-import ru.ifmo.fbsat.core.solver.iffAnd
 import ru.ifmo.fbsat.core.solver.imply
-import ru.ifmo.fbsat.core.solver.newBoolVarArray
-import ru.ifmo.fbsat.core.solver.newIntVarArray
 import ru.ifmo.fbsat.core.task.modular.extended.consecutive.ConsecutiveModularExtendedVariables
 import ru.ifmo.fbsat.core.task.single.extended.ExtendedVariables
 
@@ -14,29 +10,13 @@ fun Solver.declareGuardConditionsBfsConstraints(extendedVars: ExtendedVariables)
     with(extendedVars) {
         for (c in 1..C)
             for (k in 1..K) {
-                val bfsTransitionGuard = newBoolVarArray(P, P) { (i, j) ->
-                    if (j > i) nodeParent[c, k, j] eq i else falseVariable
-                }
-                val bfsParentGuard = newIntVarArray(P) { (j) -> 0 until j }
-
-                comment("F_p")
-                // (p[j] = i) <=> t[i, j] & AND_{r<i}( ~t[r, j] ) :: i<j
-                for (j in 1..P)
-                    for (i in 1 until j)
-                        iffAnd(bfsParentGuard[j] eq i, sequence {
-                            yield(bfsTransitionGuard[i, j])
-                            for (r in 1 until i)
-                                yield(-bfsTransitionGuard[r, j])
-                        })
-
-                comment("F_BFS(p)")
-                // p[j, i] => ~p[j+1, r] :: LB<=r<i<j<UB
+                comment("BFS for c = $c, k = $k")
                 for (j in 3 until P)
                     for (i in 2 until j)
-                        for (r in 1 until i)
+                        for (s in 1 until i)
                             imply(
-                                bfsParentGuard[j] eq i,
-                                bfsParentGuard[j + 1] neq r
+                                nodeParent[c, k, j] eq i,
+                                nodeParent[c, k, j + 1] neq s
                             )
             }
     }
