cce8de722956c610007b1a6df450dcbf38122f5b
Konstantin Chukharev <lipen00@gmail.com>
2020-04-03 20:21:43 +0300

    Fix negative literals handling in RawAssignment::get

--- fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/solver/RawAssignment.kt
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/solver/RawAssignment.kt
@@ -5,6 +5,8 @@ import com.github.lipen.multiarray.IntMultiArray
 import com.github.lipen.multiarray.MultiArray
 import com.github.lipen.multiarray.map
 import com.github.lipen.multiarray.mapToInt
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 interface RawAssignment {
     operator fun get(v: Literal): Boolean
@@ -14,7 +16,7 @@ class RawAssignment0(private val data: BooleanArray) : RawAssignment {
     override operator fun get(v: Literal): Boolean = when (v) {
         Solver.trueLiteral -> true
         Solver.falseLiteral -> false
-        else -> data[v - 1]
+        else -> data[v.absoluteValue - 1] xor (v < 0)
     }
 
     override fun toString(): String {
@@ -26,7 +28,7 @@ class RawAssignment1(private val data: BooleanArray) : RawAssignment {
     override operator fun get(v: Literal): Boolean = when (v) {
         Solver.trueLiteral -> true
         Solver.falseLiteral -> false
-        else -> data[v]
+        else -> data[v.absoluteValue] xor (v < 0)
     }
 
     override fun toString(): String {
