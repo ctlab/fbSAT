6413271004ad62300671847fc6c12163fab11da5
Konstantin Chukharev <lipen00@gmail.com>
2021-02-18 01:01:14 +0300

    Move fbsat execution into run.sh file

--- fbsat-cli/src/main/kotlin/ru/ifmo/fbsat/cli/command/randexp/Batch.kt
+++ fbsat-cli/src/main/kotlin/ru/ifmo/fbsat/cli/command/randexp/Batch.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.json.Json
 import okio.buffer
 import okio.sink
 import ru.ifmo.fbsat.core.utils.MyLogger
+import ru.ifmo.fbsat.core.utils.ensureParentExists
 import ru.ifmo.fbsat.core.utils.fbsatSerializersModule
 import ru.ifmo.fbsat.core.utils.useWith
 import ru.ifmo.fbsat.core.utils.writeln
@@ -38,12 +39,67 @@ fun generateBatch(
     val arrayAutomatonSeeds = mutableListOf<Int>()
     val arrayScenariosSeeds = mutableListOf<Int>()
     val arraySolverSeeds = mutableListOf<Int>()
+
     for (automatonSeed in automatonSeeds)
         for (scenariosSeed in scenariosSeeds)
             for (solverSeed in solverSeeds) {
                 arrayAutomatonSeeds += automatonSeed
                 arrayScenariosSeeds += scenariosSeed
                 arraySolverSeeds += solverSeed
+
+                val outDir = outBaseDir.resolve(
+                    "exp" +
+                        "_C${C}_X${X}_Z${Z}_a$automatonSeed" +
+                        "_n${n}_k${k}_s$scenariosSeed" +
+                        "_${solver}_r$solverSeed"
+                )
+                val lockFile = outDir.resolve("lock")
+                val runFile = outDir.resolve("run.sh")
+
+                val fbsatCmd = buildString {
+                    append(fbsatBin)
+                    // Note: do not forget spaces!
+                    append(" randexp")
+                    // Automaton params
+                    append(" -C $C -X $X -Z $Z --automaton-seed $automatonSeed")
+                    // Scenarios params
+                    append(" -n $n -k $k --scenarios-seed $scenariosSeed")
+                    // Inference params
+                    append(" --method extended-min -P 5")
+                    append(" --$solver --solver-seed $solverSeed")
+                    if (solver in listOf("minisat", "glucose")) {
+                        append(" --solver-rnd-freq 0.01 --solver-rnd-pol --solver-rnd-init")
+                    }
+                    append(" --outdir $outDir")
+                    // Misc params
+                    append(" --no-render-with-dot")
+                    append(" --debug")
+                }
+
+                runFile.ensureParentExists().sink().buffer().useWith {
+                    // sbatch header
+                    writeln("""
+                        #!/bin/bash
+                        #SBATCH --job-name=randexp
+                        #SBATCH --output=${outDir.resolve("slurm-%j.log")}
+                        #
+                        #SBATCH --ntasks=1
+                        #SBATCH --time=01:00:00
+                        #SBATCH --mem=1G
+                    """.trimIndent())
+                    // general info
+                    writeln("echo pwd: $(pwd); echo hostname: $(hostname); echo date: $(date -Iseconds)")
+                    // create lock file
+                    writeln().writeln("touch $lockFile")
+
+                    // run experiment
+                    writeln().writeln("srun $fbsatCmd")
+
+                    // remove lock file
+                    writeln().writeln("rm -f $lockFile")
+                    // general info
+                    writeln().writeln("echo date: $(date -Iseconds)")
+                }
             }
 
     val outDir = outBaseDir.resolve(
@@ -52,28 +108,7 @@ fun generateBatch(
             "_n${n}_k${k}_s\$scenariosSeed" +
             "_${solver}_r\$solverSeed"
     )
-    val lockFile = outDir.resolve("lock")
-
-    val fbsatCmd = buildString {
-        append(fbsatBin)
-        // Note: do not forget spaces!
-        append(" randexp")
-        // Automaton params
-        append(" -C $C -X $X -Z $Z --automaton-seed \$automatonSeed")
-        // Scenarios params
-        append(" -n $n -k $k --scenarios-seed \$scenariosSeed")
-        // Inference params
-        append(" --method extended-min -P 5")
-        append(" --$solver --solver-seed \$solverSeed")
-        if (solver in listOf("minisat", "glucose")) {
-            append(" --solver-rnd-freq 0.01 --solver-rnd-pol --solver-rnd-init")
-        }
-        append(" --outdir $outDir")
-        // Misc params
-        append(" --no-render-with-dot")
-        append(" --debug")
-    }
-    logger.debug { "fbsat command: $fbsatCmd" }
+    val runFile = outDir.resolve("run.sh")
 
     slurmFile.sink().buffer().useWith {
         // sbatch header
@@ -90,11 +125,11 @@ fun generateBatch(
         """.trimIndent())
 
         // general info
-        writeln("echo pwd: $(pwd); echo hostname: $(hostname); echo date: $(date -Iseconds)").writeln()
+        writeln("echo pwd: $(pwd); echo hostname: $(hostname); echo date: $(date -Iseconds)")
         writeln("echo SLURM_ARRAY_TASK_ID=\$SLURM_ARRAY_TASK_ID")
 
         // data for array jobs
-        writeln("""
+        writeln().writeln("""
             AUTOMATON_SEEDS=(${arrayAutomatonSeeds.joinToString(" ")})
             SCENARIOS_SEEDS=(${arrayScenariosSeeds.joinToString(" ")})
             SOLVER_SEEDS=(${arraySolverSeeds.joinToString(" ")})
@@ -104,17 +139,8 @@ fun generateBatch(
             solverSeed=${'$'}{SOLVER_SEEDS[${'$'}SLURM_ARRAY_TASK_ID]}
         """.trimIndent())
 
-        // ensure output dir exists
-        writeln().writeln("mkdir -p $outDir")
-
-        // create lock file
-        writeln().writeln("touch $lockFile")
-
         // run experiment
-        writeln().writeln("srun $fbsatCmd")
-
-        // remove lock file
-        writeln().writeln("rm -f $lockFile")
+        writeln().writeln("srun $runFile")
 
         // general info
         writeln().writeln("echo date: $(date -Iseconds)")
