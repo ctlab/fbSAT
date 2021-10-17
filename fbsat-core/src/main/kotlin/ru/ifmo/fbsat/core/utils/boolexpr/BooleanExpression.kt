8b4341389445ffe4b2728b58c959b494174201ed
Konstantin Chukharev <lipen00@gmail.com>
2021-10-18 00:32:05 +0300

    Move random-related code to a separate file

--- fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/utils/boolexpr/BooleanExpression.kt
+++ fbsat-core/src/main/kotlin/ru/ifmo/fbsat/core/utils/boolexpr/BooleanExpression.kt
@@ -3,8 +3,6 @@ package ru.ifmo.fbsat.core.utils.boolexpr
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlin.random.Random
-import kotlin.random.nextInt
 
 sealed interface BooleanExpression {
     fun eval(inputs: List<Boolean>): Boolean
@@ -42,44 +40,6 @@ sealed interface BooleanExpression {
             return args.reduce { acc, expr -> or(acc, expr) }
         }
     }
-}
-
-fun randomBooleanExpression(
-    size: Int,
-    inputNames: List<String>,
-    random: Random = Random,
-): BooleanExpression {
-    fun helper(size: Int): BooleanExpression {
-        require(size > 0) { "Size must be > 0." }
-        return when (size) {
-            1 -> {
-                val i = random.nextInt(0, inputNames.size)
-                Variable(i, inputNames[i])
-            }
-            2 -> {
-                val child = helper(1)
-                UnaryOperation(UnaryOperation.Kind.Not, child)
-            }
-            else -> when (val t = listOf("Not", "And", "Or").random(random)) {
-                "Not" -> {
-                    val child = helper(size - 1)
-                    UnaryOperation(UnaryOperation.Kind.Not, child)
-                }
-                "And", "Or" -> {
-                    // why (size - 2) ?
-                    //  subtract 1 for the current (BinaryOperation) node, and
-                    //  subtract 1 more to ensure that rhs is at least of size 1
-                    val leftSize = random.nextInt(1..(size - 2))
-                    val lhs = helper(leftSize)
-                    val rhs = helper(size - 1 - leftSize)
-                    val kind = BinaryOperation.Kind.valueOf(t)
-                    BinaryOperation(kind, lhs, rhs)
-                }
-                else -> error("Unexpected type $t")
-            }
-        }
-    }
-    return helper(size)
 }
 
 fun main() {
