d85d1e7406c6b1a99ce9e5b2890979d85108a739
Konstantin Chukharev <lipen00@gmail.com>
2021-10-22 17:46:54 +0300

    Update mutex synthesis

--- fbsat-cli/src/main/kotlin/ru/ifmo/fbsat/cli/entry/braccio/mutex/Mutex.kt
+++ fbsat-cli/src/main/kotlin/ru/ifmo/fbsat/cli/entry/braccio/mutex/Mutex.kt
@@ -14,8 +14,10 @@ import ru.ifmo.fbsat.core.scenario.OutputEvent
 import ru.ifmo.fbsat.core.scenario.OutputValues
 import ru.ifmo.fbsat.core.scenario.ScenarioElement
 import ru.ifmo.fbsat.core.scenario.positive.PositiveScenario
+import ru.ifmo.fbsat.core.scenario.positive.PositiveScenarioTree
 import ru.ifmo.fbsat.core.scenario.preprocessed
 import ru.ifmo.fbsat.core.task.Inferrer
+import ru.ifmo.fbsat.core.task.single.basic.basicMin
 import ru.ifmo.fbsat.core.utils.Globals
 import ru.ifmo.fbsat.core.utils.MyLogger
 import ru.ifmo.fbsat.core.utils.lineSequence
@@ -53,6 +55,52 @@ data class LogLine(
     }
 }
 
+private fun readScenarioFromLog(
+    file: File,
+    inputNames: List<String>,
+    outputNames: List<String>,
+    outputNamesBoolean: List<String>,
+    outputNamesIntWithDomain: List<Pair<String, List<Int>>>,
+): PositiveScenario {
+    val interestingLines = file.source().buffer().lineSequence().filter {
+        it.startsWith("[iter #")
+    }.toList().let {
+        // Remove last line (because it is probably corrupted)
+        it.subList(0, it.size - 1)
+    }
+    // println("Interesting log lines:")
+    // for (line in interestingLines) {
+    //     val logLine = LogLine.from(line)
+    //     println("  - $logLine")
+    // }
+    val logLines = interestingLines.map { LogLine.from(it) }
+    val elements = logLines
+        .filter { it.index == 1 }
+        .zipWithNext { a, b ->
+            val outputValuesBoolean = outputNamesBoolean.associateWith { name ->
+                b.data.getValue(name).toBool()
+            }
+            val outputValuesInt = outputNamesIntWithDomain.flatMap { (name, domain) ->
+                domain.map { v ->
+                    Pair("${name}_$v", (b.data.getValue(name).toInt() == v))
+                }
+            }.toMap()
+            val map = outputValuesBoolean + outputValuesInt
+            val outputValues = outputNames.map { map.getValue(it) }
+            ScenarioElement(
+                InputAction(
+                    event = InputEvent("REQ"),
+                    values = InputValues(inputNames.map { a.data.getValue(it).toBool() })
+                ),
+                OutputAction(
+                    event = OutputEvent("CNF"),
+                    values = OutputValues(outputValues)
+                )
+            )
+        }
+    return PositiveScenario(elements.preprocessed)
+}
+
 fun main() {
     Globals.IS_DEBUG = true
 
@@ -75,48 +123,61 @@ fun main() {
         "is_done_m5",
         "is_done_m6",
     )
-    val outputNames = listOf(
-        "go_base",
-        "go_shoulder",
-        "go_elbow",
-        "go_wrist_ver",
-        "go_wrist_rot",
-        "go_gripper",
-        "active",
+    val outputNamesBoolean = listOf(
         "want_to_release",
         "want_to_acquire",
     )
+    val anglesBase = listOf(0, 90, 120)
+    val anglesShoulder = listOf(65, 80, 100, 115, 140)
+    val anglesElbow = listOf(0, 180)
+    val anglesWristVer = listOf(10, 15, 165, 170)
+    val anglesWristRot = listOf(0, 90)
+    val anglesGripper = listOf(15, 73)
+    val outputNamesInt =
+        anglesBase.map { "go_base_$it" } +
+            anglesShoulder.map { "go_shoulder_$it" } +
+            anglesElbow.map { "go_elbow_$it" } +
+            anglesWristVer.map { "go_wrist_ver_$it" } +
+            anglesWristRot.map { "go_wrist_rot_$it" } +
+            anglesGripper.map { "go_gripper_$it" }
+    val outputNames = outputNamesBoolean + outputNamesInt
+
     val fileLog = File("data/braccio/mutex/serial_mutex_i2c_button1.log")
-    val interestingLines = fileLog.source().buffer().lineSequence().filter {
-        it.startsWith("[iter #")
-    }.toList().let {
-        // Remove last line (because it is probably corrupted)
-        it.subList(0, it.size - 1)
-    }
-    for (line in interestingLines) {
-        val logLine = LogLine.from(line)
-        println(logLine)
-    }
-    val logLines = interestingLines.map { LogLine.from(it) }
-    val elements = logLines
-        .filter { it.index == 1 }
-        .zipWithNext { a, b ->
-            ScenarioElement(
-                InputAction(
-                    event = InputEvent("REQ"),
-                    values = InputValues(inputNames.map { a.data.getValue(it).toBool() })
-                ),
-                OutputAction(
-                    event = OutputEvent("CNF"),
-                    values = OutputValues(outputNames.map { b.data.getValue(it).toBool() })
-                )
-            )
-        }
-    val scenario = PositiveScenario(elements.preprocessed)
-    println("scenario = $scenario")
+    val scenario = readScenarioFromLog(
+        file = fileLog,
+        inputNames = inputNames,
+        outputNames = outputNames,
+        outputNamesBoolean = outputNamesBoolean,
+        outputNamesIntWithDomain = listOf(
+            "go_base" to anglesBase,
+            "go_shoulder" to anglesShoulder,
+            "go_elbow" to anglesElbow,
+            "go_wrist_ver" to anglesWristVer,
+            "go_wrist_rot" to anglesWristRot,
+            "go_gripper" to anglesGripper,
+        )
+    )
+    val scenarios = listOf(scenario)
+    val scenarioTree = PositiveScenarioTree.fromScenarios(
+        scenarios = scenarios,
+        inputNames = inputNames,
+        outputNames = outputNamesBoolean,
+        inputEvents = listOf(InputEvent("REQ")),
+        outputEvents = listOf(OutputEvent("CNF"))
+    )
+    scenarioTree.printStats()
 
     // infer
-    // TODO
+    val automaton = inferrer.basicMin(scenarioTree)
+
+    if (automaton != null) {
+        logger.info("Inferred automaton:")
+        automaton.pprint()
+        logger.info("Inferred automaton has:")
+        automaton.printStats()
+    } else {
+        logger.error("Inference failed")
+    }
 
     logger.info("End time: ${DateTime.nowLocal().format(dateTimeFormat)}")
     logger.info("All done in %.3f seconds".format(timeSince(timeStart).seconds))
